ff4c36b335f554b11991aaf105f5a162f70198f3
Anik Sen <75422517+senanik360@users.noreply.github.com>
2022-07-01 14:17:17 +0600

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("\t\t--------------------------");
+        System.out.println("\t\t  Welcome TO Our Shop.");
+        System.out.println("\t\t--------------------------");
+
+        Start st = new Start();
+        st.start0();
+        st.start1();
+    }
+}
---
+++ src/Start.java
@@ -0,0 +1,51 @@
+
+import java.util.Scanner;
+
+public class Start extends SignUpIn {
+
+    Choice ch = new Choice();
+    ShopDetails shop = new ShopDetails();
+
+    int f, k;
+
+    void start0() {
+        entry();
+//        SignUp();
+//        SignIn();
+        welcomeAgain();
+    }
+
+    void start1() {
+        System.out.println("1.Products   -| |-   2.About Shop");
+        do {
+            f = 0;
+            try {
+                Scanner input = new Scanner(System.in);
+                k = 0;
+                int n = input.nextInt();
+                if (n == 1) {
+                    if (ch == null) {
+                        ch = new Choice();
+                    }
+                    ch.choice1();
+                } else if (n == 2) {
+                    if (shop == null) {
+                        shop = new ShopDetails();
+                    }
+                    shop.shopDetails1();
+                } else {
+                    f = 1;
+                    System.out.println("Invalid Input. Try again");
+                }
+            } catch (Exception e) {
+                k = 1;
+                System.out.println("Exception : " + e);
+            } finally {
+                if (k == 1) {
+                    System.out.println("Invalid Input. Try again");
+                }
+            }
+        } while (f == 1 || k == 1);
+    }
+
+}
